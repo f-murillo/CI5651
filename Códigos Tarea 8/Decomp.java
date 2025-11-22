98a284dfb804f1c9539bcb6a65f894f5c0f28221
f-murillo <murillo.franc@gmail.com>
2025-11-21 21:24:32 -0400

    Add files via upload

--- Códigos Tarea 8/Decomp.java
+++ Códigos Tarea 8/Decomp.java
@@ -1,22 +1,28 @@
-public class Decomp {
+public class Decomp{
     // Clase para los numeros complejos (para el FFT)
-    static class Complex {
+    static class Complex{
         double re, im;
         Complex(double r, double i) { re = r; im = i; }
 
-        Complex add(Complex o) { return new Complex(re + o.re, im + o.im); }
-        Complex sub(Complex o) { return new Complex(re - o.re, im - o.im); }
-        Complex mul(Complex o) { return new Complex(re*o.re - im*o.im, re*o.im + im*o.re); }
+        Complex add(Complex o){
+            return new Complex(re + o.re, im + o.im);
+        }
+        Complex sub(Complex o){
+            return new Complex(re - o.re, im - o.im);
+        }
+        Complex mul(Complex o){
+            return new Complex(re*o.re - im*o.im, re*o.im + im*o.re);
+        }
     }
 
     // FFT
-    static Complex[] fft(Complex[] a, boolean invert) {
+    static Complex[] fft(Complex[] a, boolean invert){
         int n = a.length;
-        if (n == 1) return new Complex[]{ a[0] };
+        if(n == 1) return new Complex[]{ a[0] };
 
         Complex[] aEven = new Complex[n/2];
         Complex[] aOdd  = new Complex[n/2];
-        for (int i = 0; i < n/2; i++) {
+        for(int i = 0; i < n/2; i++){
             aEven[i] = a[2*i];
             aOdd[i]  = a[2*i+1];
         }
@@ -29,7 +35,7 @@ public class Decomp {
         Complex wn = new Complex(Math.cos(ang), Math.sin(ang));
         Complex w = new Complex(1, 0);
 
-        for (int j = 0; j < n/2; j++) {
+        for(int j = 0; j < n/2; j++){
             Complex u = s[j];
             Complex v = t[j].mul(w);
             r[j] = u.add(v);
@@ -41,33 +47,33 @@ public class Decomp {
     }
 
     // Convolucion con el FFT
-    static int[] convolution(int[] f) {
+    static int[] convolution(int[] f){
         int n = 1;
-        while (n < 2*f.length) n <<= 1;
+        while(n < 2*f.length) n <<= 1;
 
         Complex[] a = new Complex[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             a[i] = new Complex(i < f.length ? f[i] : 0, 0);
         }
 
         Complex[] A = fft(a, false);
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             A[i] = A[i].mul(A[i]); // convolucion consigo misma
         }
         Complex[] convC = fft(A, true);
 
         int[] conv = new int[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             conv[i] = (int)Math.round(convC[i].re / n);
         }
         return conv;
     }
 
     // Preprocesamiento: criba modificada para el numero de divisores
-    static int[] F(int N) {
+    static int[] F(int N){
         int[] f = new int[N+1];
-        for (int d = 1; d <= N; d++) {
-            for (int m = d; m <= N; m += d) {
+        for(int d = 1; d <= N; d++){
+            for(int m = d; m <= N; m += d) {
                 f[m]++;
             }
         }
@@ -80,10 +86,11 @@ public class Decomp {
         int[] conv = convolution(f);
 
         int max = 0;
-        for (int X = 1; X <= N; X++) {
-            if (conv[X] > max) max = conv[X];
+        for(int X = 1; X <= N; X++){
+            if(conv[X] > max) max = conv[X];
         }
 
         return max;
     }
 }
+
